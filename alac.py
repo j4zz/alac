b427666a69b4f94024778110c128977679741bd4
jazz <89894457+j4zz@users.noreply.github.com>
2024-07-31 22:46:39 -0400

    Add files via upload

---
+++ alac.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+def paths(directory: str):
+    for dirpath,_,filenames in os.walk(directory):
+        for f in filenames:
+            yield os.path.abspath(os.path.join(dirpath, f))
+
+filepaths: list[str] = paths(os.getcwd())       
+for file in filepaths:
+    if file.endswith(".flac"):
+        name = os.path.basename(file)
+        subprocess.run(f'ffmpeg -nostdin -i "{file}" -c:a alac -c:v copy "{name}.m4a"')
+        os.remove(file)
